7b59ca673a24eb2a7beda73f099731c2c0c0bfc6
Prashant Kalkar <kalkar.prashant@gmail.com>
2018-04-25 13:04:41 +0530

    Added sample spring test

---
+++ src/test/java/com/ee/orderservice/OrderServiceTest.java
@@ -0,0 +1,14 @@
+package com.ee.orderservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class OrderServiceTest {
+
+    @Test
+    public void springWiringTest() {
+
+    }
+}
