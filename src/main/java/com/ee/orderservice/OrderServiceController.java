367a6ddaa06112ea34fad18ce45a870ada72ee19
Prashant Kalkar <kalkar.prashant@gmail.com>
2018-04-25 12:58:04 +0530

    Added code for basic order endpoint to create order. Made order service fetch details from user server for order creation.

---
+++ src/main/java/com/ee/orderservice/OrderServiceController.java
@@ -0,0 +1,69 @@
+package com.ee.orderservice;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OrderServiceController {
+
+    @PostMapping(path = "/order", consumes = "application/json", produces = "application/json")
+    public String placeOrder(@RequestBody OrderDetails orderDetails) {
+        RestTemplate restTemplate = new RestTemplate();
+        User userDetails = restTemplate.getForObject("http://localhost:8080/user/myUserId", User.class);
+        System.out.println("User details obtained: " + userDetails);
+
+        return "{\"orderId\" : \"7998289398179\"}";
+    }
+
+    public static class OrderDetails {
+
+        private String productId;
+        private String quantity;
+        private String userId;
+
+        public OrderDetails(String productId, String quantity, String userId) {
+            this.productId = productId;
+            this.quantity = quantity;
+            this.userId = userId;
+        }
+
+        @Override
+        public String toString() {
+            return "OrderDetails{" +
+                    "productId='" + productId + '\'' +
+                    ", quantity='" + quantity + '\'' +
+                    ", userId='" + userId + '\'' +
+                    '}';
+        }
+    }
+
+
+    public static class User {
+        private String name;
+        private String email;
+        private String address;
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "name='" + name + '\'' +
+                    ", email='" + email + '\'' +
+                    ", address='" + address + '\'' +
+                    '}';
+        }
+    }
+}
